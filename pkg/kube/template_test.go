506fb9fbeaff612c3a90f901decadf816a0e3bfb
Josh Kayani <joshkayani@gmail.com>
2020-11-19 11:25:52 -0500

    tests: For Service and error-path Deployment

--- pkg/kube/template_test.go
+++ pkg/kube/template_test.go
@@ -53,6 +53,54 @@ func TestToYAML_Deployment(t *testing.T) {
 	}
 }
 
+func TestToYAML_Service(t *testing.T) {
+	d := ServiceTemplate{
+		Resource{
+			templateData: map[string]interface{}{
+				"kind": "Service",
+				"metadata": map[string]interface{}{
+					"namespace": "default",
+					"name":      "<name>",
+				},
+				"spec": map[string]interface{}{
+					"selector": map[string]interface{}{
+						"app": "<name>",
+					},
+					"ports": []interface{}{
+						map[string]interface{}{
+							"port": "<port>",
+						},
+					},
+				},
+			},
+			vaultData: map[string]interface{}{
+				"name": "my-app",
+				"port": 8080,
+			},
+		},
+	}
+
+	err := d.Replace()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	expectedData, err := ioutil.ReadFile("../../fixtures/output/small-service.yaml")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	expected := string(expectedData)
+	actual, err := d.ToYAML()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	if !strings.Contains(actual, expected) {
+		t.Fatalf("expected YAML:\n%s\nbut got:\n%s\n", expected, actual)
+	}
+}
+
 func TestToYAML_Secret(t *testing.T) {
 	d := SecretTemplate{
 		Resource{
@@ -134,5 +182,48 @@ func TestToYAML_ConfigMap(t *testing.T) {
 
 	if !strings.Contains(actual, expected) {
 		t.Fatalf("expected YAML:\n%s\nbut got:\n%s\n", expected, actual)
+	}
+}
+
+func TestToYAML_DeploymentBad(t *testing.T) {
+	d := DeploymentTemplate{
+		Resource{
+			templateData: map[string]interface{}{
+				"metadata": map[string]interface{}{
+					"namespace": "default",
+					"name":      "<name>",
+				},
+				"spec": map[string]interface{}{
+					"replicas":        "<replicas>",
+					"minReadySeconds": "<minReadySeconds>",
+					"template": map[string]interface{}{
+						"metadata": map[string]interface{}{
+							"labels": map[string]interface{}{
+								"app": "<name>",
+							},
+						},
+					},
+				},
+			},
+			vaultData: map[string]interface{}{
+				"replicas":        3,
+				"minReadySeconds": "one hundred",
+				"name":            "!!@#@.---.",
+			},
+		},
+	}
+
+	err := d.Replace()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	actual, err := d.ToYAML()
+	if err == nil {
+		t.Fatalf("Expected ToYAML error but got %s", actual)
+	}
+
+	if !strings.Contains(err.Error(), "DeploymentSpec.spec.minReadySeconds of type int32") {
+		t.Fatalf("Expected error with minReadySeconds, got: %s", err.Error())
 	}
 }
