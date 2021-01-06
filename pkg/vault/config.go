baf2f4ae432526395c6627ba26fc0b54dcc6d757
Josh Kayani <joshkayani@gmail.com>
2021-01-06 16:01:32 -0500

    refactor: Use Viper auto-env with prefix

--- pkg/vault/config.go
+++ pkg/vault/config.go
@@ -17,14 +17,8 @@ type Config struct {
 }
 
 func setupViper() {
-	viper.BindEnv("VaultType", "AVP_TYPE")
-	viper.BindEnv("Address", "AVP_VAULT_ADDR")
-	viper.BindEnv("PathPrefix", "AVP_PATH_PREFIX")
-	viper.BindEnv("AuthType", "AVP_AUTH_TYPE")
-	viper.BindEnv("RoleID", "AVP_ROLE_ID")
-	viper.BindEnv("SecretID", "AVP_SECRET_ID")
-	viper.BindEnv("GithubToken", "AVP_GITHUB_TOKEN")
-	viper.BindEnv("IBMCloudAPIKey", "AVP_IBM_API_KEY")
+	viper.SetEnvPrefix("AVP")
+	viper.AutomaticEnv()
 }
 
 // NewConfig returns a new Config struct
@@ -32,15 +26,15 @@ func NewConfig() (*Config, error) {
 	setupViper()
 
 	config := &Config{
-		Address:    viper.GetString("Address"),
-		PathPrefix: viper.GetString("PathPrefix"),
+		Address:    viper.GetString("VAULT_ADDR"),
+		PathPrefix: viper.GetString("PATH_PREFIX"),
 	}
 
 	var httpClient = &http.Client{
 		Timeout: 10 * time.Second,
 	}
 
-	apiClient, err := api.NewClient(&api.Config{Address: viper.GetString("Address"), HttpClient: httpClient})
+	apiClient, err := api.NewClient(&api.Config{Address: viper.GetString("VAULT_ADDR"), HttpClient: httpClient})
 	if err != nil {
 		return nil, err
 	}
@@ -49,29 +43,29 @@ func NewConfig() (*Config, error) {
 		VaultAPIClient: apiClient,
 	}
 
-	auth := viper.GetString("AuthType")
+	auth := viper.GetString("AUTH_TYPE")
 
-	switch viper.GetString("VaultType") {
+	switch viper.GetString("TYPE") {
 	case "vault":
 		switch auth {
 		case "approle":
-			if viper.IsSet("RoleID") && viper.IsSet("SecretID") {
+			if viper.IsSet("ROLE_ID") && viper.IsSet("SECRET_ID") {
 				config.Type = &AppRole{
-					RoleID:   viper.GetString("RoleID"),
-					SecretID: viper.GetString("SecretID"),
+					RoleID:   viper.GetString("ROLE_ID"),
+					SecretID: viper.GetString("SECRET_ID"),
 					Client:   client,
 				}
 			} else {
-				return nil, errors.New("RoleID and SecretID for approle authentication cannot be empty")
+				return nil, errors.New("ROLE_ID and SECRET_ID for approle authentication cannot be empty")
 			}
 		case "github":
-			if viper.IsSet("GithubToken") {
+			if viper.IsSet("GITHUB_TOKEN") {
 				config.Type = &Github{
-					AccessToken: viper.GetString("GithubToken"),
+					AccessToken: viper.GetString("GITHUB_TOKEN"),
 					Client:      client,
 				}
 			} else {
-				return nil, errors.New("GithubToken for github authentication cannot be empty")
+				return nil, errors.New("GITHUB_TOKEN for github authentication cannot be empty")
 			}
 		default:
 			return nil, errors.New("Must provide a supported Authentication Type")
@@ -79,13 +73,13 @@ func NewConfig() (*Config, error) {
 	case "secretmanager":
 		switch auth {
 		case "iam":
-			if viper.IsSet("IBMCloudAPIKey") {
+			if viper.IsSet("IBM_API_KEY") {
 				config.Type = &SecretManager{
-					IBMCloudAPIKey: viper.GetString("IBMCloudAPIKey"),
+					IBMCloudAPIKey: viper.GetString("IBM_API_KEY"),
 					Client:         client,
 				}
 			} else {
-				return nil, errors.New("IBMCloudAPIKey for iam authentication cannot be empty")
+				return nil, errors.New("IBM_API_KEY for iam authentication cannot be empty")
 			}
 		default:
 			return nil, errors.New("Must provide a supported Authentication Type")
