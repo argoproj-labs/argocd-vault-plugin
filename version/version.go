327caa01d5475fc95287e553b26220a47156703e
TJ Miller <millert@us.ibm.com>
2021-06-08 11:40:53 -0700

    ci: add release automation

--- version/version.go
+++ version/version.go
@@ -2,5 +2,11 @@ package version
 
 var (
 	// Version is the argocd-vault-plugin version.
-	Version = "v1.0.0"
+	Version string
+
+	// BuildDate is the date argocd-vault-plugin was built
+	BuildDate string
+
+	// CommitSHA is the commit argocd-vault-plugin was built from
+	CommitSHA string
 )
