89d068606e5c4bb1beeea8d5f9b8ab2eb42e0b5e
Floris Van den Abeele <floris@vdna.be>
2021-06-16 16:19:57 +0200

    fix: remove unnecessary env vars check for AWS credentials (IBM#148)
    Just use the default credentials chain present in the go AWS SDK, which
    supports other authentication methods besides env variables.

--- pkg/types/constants.go
+++ pkg/types/constants.go
@@ -2,20 +2,18 @@ package types
 
 const (
 	// Environment Variable Constants
-	EnvAvpType            = "AVP_TYPE"
-	EnvAvpRoleID          = "AVP_ROLE_ID"
-	EnvAvpSecretID        = "AVP_SECRET_ID"
-	EnvAvpAuthType        = "AVP_AUTH_TYPE"
-	EnvAvpGithubToken     = "AVP_GITHUB_TOKEN"
-	EnvAvpK8sRole         = "AVP_K8S_ROLE"
-	EnvAvpK8sMountPath    = "AVP_K8S_MOUNT_PATH"
-	EnvAvpK8sTokenPath    = "AVP_K8S_TOKEN_PATH"
-	EnvAvpIBMAPIKey       = "AVP_IBM_API_KEY"
-	EnvAvpKvVersion       = "AVP_KV_VERSION"
-	EnvAvpPathPrefix      = "AVP_PATH_PREFIX"
-	EnvAWSAccessKey       = "AWS_ACCESS_KEY_ID"
-	EnvAWSSecretAccessKey = "AWS_SECRET_ACCESS_KEY"
-	EnvAWSRegion          = "AWS_REGION"
+	EnvAvpType         = "AVP_TYPE"
+	EnvAvpRoleID       = "AVP_ROLE_ID"
+	EnvAvpSecretID     = "AVP_SECRET_ID"
+	EnvAvpAuthType     = "AVP_AUTH_TYPE"
+	EnvAvpGithubToken  = "AVP_GITHUB_TOKEN"
+	EnvAvpK8sRole      = "AVP_K8S_ROLE"
+	EnvAvpK8sMountPath = "AVP_K8S_MOUNT_PATH"
+	EnvAvpK8sTokenPath = "AVP_K8S_TOKEN_PATH"
+	EnvAvpIBMAPIKey    = "AVP_IBM_API_KEY"
+	EnvAvpKvVersion    = "AVP_KV_VERSION"
+	EnvAvpPathPrefix   = "AVP_PATH_PREFIX"
+	EnvAWSRegion       = "AWS_REGION"
 
 	// Backend and Auth Constants
 	VaultBackend             = "vault"
